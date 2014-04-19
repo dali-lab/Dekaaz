191eb76b1ec84be7eaf376ad0d29bf03503c6389
Barry Chen <Barry.Y.Chen.16@dartmouth.edu>
2014-04-19 18:17:35 -0400

    save

--- dekaaz.js
+++ dekaaz.js
@@ -1,8 +1,175 @@
+Parse.initialize("m5nklTTbXDmvXRxCiOMIt9SB6U0Iz2uKZ9AeGnXq", "8brbnvrjnVZiIsOk8jzS98f3yVLTtza17J2zqGBx");
+
+var soundFile;
+
+ function croakCallbackFunction(croak_id,croakmp3source,callbackCode)
+ {
+    if(callbackCode==1){
+           // alert('talk_id '+croak_id);
+           // var str = '<audio controls><source src="' + croakmp3source + '" type="audio/mpeg">Your browser does not support this audio format.</audio>';
+           var str = '<audio src="' + croakmp3source + '" controls="controls"></audio>';
+           // alert("HI");
+           soundFile = croakmp3source;
+		   $('#audio').html(str);
+           // alert("HEY??");
+        //    var audio = $("audio");
+        //    audio[0].pause();
+    	   // audio[0].load();
+        //    // $('audio').show();
+        //    audio[0].play();
+        //    alert("HERE");
+           // alert('talkmp3source '+croakmp3source);
+
+    }
+ }
+
+function logoutFunc() {
+	$("#logout-button").attr("disabled", "disabled");
+
+	Parse.User.logOut();
+	window.location.href = "signup.html";
+}
+
+function getRealUsername(user) {
+
+	return user.getUsername();
+}
+
+function attachUserLinks() {
+	$(".user").click(function() {
+		setCookie("user_account", $(this).attr('id'), 1);
+		window.location.href = "account.php";
+	});
+}
+
+var currentDekaaz = 0;
+
+function individualizeDekaazs(num) {
+	// num = 100;
+ //    var options = {
+ //        currentPage: 3,
+ //        totalPages: Math.ceil(num/10)
+ //    }
+ // 	$('.pagination ul').empty();
+ //    var x = '<li><a href="javascript:goToPreviousPage()">←</a></li>';
+ //    $('.pagination ul').append(x);
+ // 	// x = '<li class="active" id="' + 1 + '"><a href="javascript:goToPage(1)">1</a></li>';
+ // 	// $('.pagination ul').append(x);
+ // 	// if(Math.floor(num/10) == 0) {
+ // 	// 	x = '<li class="active" id="1"><a href="javascript:goToPage(' + 1 + ')">' + 1 + '</a></li>';
+ //  // 		$('.pagination ul').append(x);
+ // 	// }
+ // 	for (var i=1; i<=Math.ceil(num/10); i++)
+ //  	{
+ //  		if(i == 1) {
+ //  			x = '<li class="active" id="' + i + '"><a href="javascript:goToPage(' + i + ')">' + i + '</a></li>';
+ //  			$('.pagination ul').append(x);
+ //  			continue;
+ //  		}
+ //  		x = '<li id="' + i + '"><a href="javascript:goToPage(' + i + ')">' + i + '</a></li>';
+ // 		$('.pagination ul').append(x);
+ //  	}
+ //    // $('.pagination ul').append('<li class="disabled"><a href="#">...</a></li>');
+	// $('.pagination ul').append('<li><a href="javascript:goToAfterPage(' + Math.floor(num/10) + ')">→</a></li>');
+    // $('#example').bootstrapPaginator(options);
+    $('#incomplete-items li').slice(currentDekaaz+1).hide();
+    $('#incomplete-items li:eq( ' + currentDekaaz + ' )').show();
+
+    $(window).keydown(function(e){
+	    if(e.which === 39){
+	    	// alert("DETECTED");
+	        if(currentDekaaz<num-1){
+	            $('#incomplete-items li:eq( ' + currentDekaaz + ' )').hide();
+	            currentDekaaz = currentDekaaz+1;
+	            $('#incomplete-items li:eq( ' + currentDekaaz + ' )').show();
+	        }
+	    }
+	    else if(e.which === 37){
+	    	// alert("DETECTED2");
+	        if(currentDekaaz>0){
+	            $('#incomplete-items li:eq( ' + currentDekaaz + ' )').hide();
+	            currentDekaaz = currentDekaaz-1;
+	            $('#incomplete-items li:eq( ' + currentDekaaz + ' )').show();
+	        }
+	    }
+	});
+    
+
+    // attachPaginationEvents(Math.ceil(num/10));
+}
+
+function goToPage(num) {
+	$('#incomplete-items li').slice(0, num*10-10).hide();
+	$('#incomplete-items li').slice(num*10-10, num*10).show();
+	$('#incomplete-items li').slice(num*10).hide();
+	$('.active').removeClass('active');
+	$('#' + num).addClass('active');
+	
+}
+
+function goToPreviousPage() {
+	var num = $('.active').attr('id');
+	if(num-1 >= 1) {
+		goToPage(num-1);
+	}
+}
+
+function goToAfterPage(maxnum) {
+	// alert(maxnum);
+	var num = parseInt($('.active:last').attr('id'));
+	// alert(maxnum);
+	// alert(num);
+	// alert(num+1 <= maxnum);
+	// alert(num+1);
+	// alert(maxnum);
+	if(num+1 <= maxnum) {
+		// alert(maxnum);
+		// alert(num+1);
+		goToPage(num+1);
+	}
+}
+
+function attachPaginationEvents(num) {
+ 	for (var i=1; i<=Math.floor(num/10); i++)
+  	{
+  		$('#' + i).click(function() {
+  			return false;
+  		});
+  	}
+}
+
+function getCookie(cname)
+{
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) 
+	  {
+	  var c = ca[i].trim();
+	  if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+	  }
+	return "";
+}
+
+function setCookie(cname,cvalue,exdays)
+{
+	var d = new Date();
+	d.setTime(d.getTime()+(exdays*24*60*60*1000));
+	var expires = "expires="+d.toGMTString();
+	document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
 YUI().use('node', function(Y) {
 	
 	var Dekaaz, 
 	query,
+	noTasksMessage = Y.one('#no-incomplete-message'),
+	submitBtn = Y.one("#poem-submit"),
+	incompleteItemList = Y.one('#incomplete-items'),
 	accountInfo = Y.one('body'),
+	completeItemList = Y.one('#complete-items'),
+	input1 = Y.one("#poem-input-1"),
+	input2 = Y.one("#poem-input-2"),
+	input3 = Y.one("#poem-input-3");
 
 	/* Top Navigation Bar: when the page is ready, get the current user's username, and pass the username to the 
 	script tag identified by '#account-info', which will generate HTML code. This HTML code is saved in the content
@@ -56,4 +223,160 @@ YUI().use('node', function(Y) {
 		}
 
 	});
+
+	function populate(val, owner_param, owner_id_param) {
+
+		if(owner_id_param == null) {
+			var content = Y.Lang.sub(Y.one('#todo-items-template-no-account').getHTML(), {
+				line1: val.get('line1'),
+				line2: val.get('line2'),
+				line3: val.get('line3'),
+				owner: owner_param,
+				createdAt: val.get('createdAt'),
+				audio: val.get('Sound'),
+				id: val.id,
+			});
+			incompleteItemList.prepend(content);
+		} else {
+			var content = Y.Lang.sub(Y.one('#todo-items-template').getHTML(), {
+				line1: val.get('line1'),
+				line2: val.get('line2'),
+				line3: val.get('line3'),
+				owner: owner_param,
+				owner_id: owner_id_param,
+				createdAt: val.get('createdAt'),
+				audio: val.get('Sound'),
+				id: val.id,
+			});
+			incompleteItemList.prepend(content);
+		}
+
+		// The following three variables are arrays of integers that hold the syllable
+		// counts for each word of each line of the Dekaaz
+		var first_line_syllables = val.get('line1syll');
+		var second_line_syllables = val.get('line2syll');
+		var third_line_syllables = val.get('line3syll');
+
+		// This line sets the image. I have set it to be Logo.png, but you can
+		// set the image with Processing, given the three arrays of int's directly above.
+		$('#incomplete-items:first>li img').attr('src', 'images/Logo.png');
+	}
+
+	/* Search Dekaazs */
+	$(".dekaaz_search").submit(function() {
+		Dekaaz = Parse.Object.extend("Dekaaz");
+		query = new Parse.Query(Dekaaz);
+		
+		query.descending('createdAt');
+		/* get the search query, and put it in str; if the search query has only spaces, then do nothing and return false */
+		var str = $("#tfnewsearch .tftextinput").val();
+		
+		/* split the search query (based on whitespace) into an array of Strings.
+		Each Dekaaz in the database has a "words" attribute, which is an array of strings containing the keywords of
+		each Dekaaz. Look at "/dekaaz/MyCloudCode/cloud/main.js" file to see exactly how the words array is generated based on a given Dekaaz.
+		Use the Parse.Query object's containedIn method to search the database for Dekaaz's whose words attribute contains
+		the words in the search query.
+		Empty the current list of Dekaazs.
+		For each of the results of the query, load the HTML under the '#todo-items-template' script tag into the content var.
+		Now that content var has all the HTML for the Dekaaz's, prepend it to the incompleteItemList, 
+		which is just the '#incomplete-items' tag in "index.html"
+
+		Later on, we might want to do "Or" search queries; refer to site here: https://www.parse.com/questions/search-full-text-search-and-or-queries
+		 */
+
+		var array;
+		Parse.Cloud.run('averageStars', {"query" : str}, {
+		  success: function(data) {
+		  	array = data;
+		    query.containedIn("words", array);
+		    query.include("username");
+			query.find({
+			  success: function(results) {
+			  	console.log(results);
+			  	incompleteItemList.empty();
+			  	Y.Array.each(results, function(val, i, arr) {
+			  		// console.log("results length");
+			  		// console.log(results.length);
+			  		var author = val.get('parent');
+			  		// console.log(author.getUsername() + " ; " + author.id);
+			  		if(author != null) {
+				  		author.fetch({
+						  success: function(author) {
+						    var author_name = author.getUsername();
+						  	populate(val, author.getUsername(), author.id);
+						  	if(i == results.length - 1) {
+								paginateDekaazs(results.length);
+							}
+						  }
+						});
+					} else {
+						populate(val, "Unknown", null);
+						if(i == results.length - 1) {
+							paginateDekaazs(results.length);
+						}
+					}
+
+				});
+			  },
+			  error: function(error) {
+			    alert("Error when retrieving: " + error.code + " " + error.message);
+			  }
+			});
+		  },
+		  error: function(error) {
+		  	alert(error.message);
+		  }
+		});
+
+		return false;
+
+		
+	});
+	
+	
+	
+
+	/* Dekaaz Listing: Retrieve the 10 most recent Dekaazs, and load the 
+	'#todo-items-template' which can be found in "index.html", passing in the Dekaaz's */
+	Dekaaz = Parse.Object.extend("Dekaaz");
+	query = new Parse.Query(Dekaaz);
+	// query.limit(10);
+	query.descending('createdAt');
+	query.find({
+	  success: function(results) {
+			if (results.length > 0) {
+				noTasksMessage.addClass('hidden');
+			}
+			//Append each of the incomplete tasks to the Incomplete List
+			Y.Array.each(results, function(val, i, arr) {
+
+
+			  		var author = val.get('parent');
+			  		if(author != null) {
+				  		author.fetch({
+						  success: function(author) {
+						    var author_name = author.getUsername();
+
+						    populate(val, author_name, author.id);
+							attachUserLinks();
+							if(i == results.length - 1) {
+								individualizeDekaazs(results.length);
+							}
+						  }
+						});
+					} else {
+						populate(val, "Unknown", null);
+						attachUserLinks();
+						if(i == results.length - 1) {
+							individualizeDekaazs(results.length);
+						}
+					}
+			});
+			
+	  },
+	  error: function(error) {
+	    alert("Error when retrieving Todos: " + error.code + " " + error.message);
+	  }
+	});
+	
 });
---
+++ dekaazpage.php
@@ -0,0 +1,125 @@
+<?php include_once("header.php"); ?>
+
+<ul id="single-item">
+	<li id="no-item-message"></li>
+</ul>
+
+<script id="todo-items-template" type="x/handlebars">
+<li class="list-item" id="{id}">
+
+
+<div style="display: inline-block; width: 47%; ">
+{line1}<br/>{line2}<br/>{line3}<br/>Author: <a id="{owner_id}" class="user">{owner}</a><br/>
+<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+<audio controls><source src="{audio}" type="audio/mpeg">Your browser does not support this audio format.</audio>
+</div>
+<div style="display: inline-block; width: 47%; vertical-align: top; float: right;">
+<img src="images/form.png" style="max-height: 100px; max-width: 100px; float: right;"/>
+
+</div>
+
+</li>
+</script>
+<script id="todo-items-template-no-account" type="x/handlebars">
+<li class="list-item" id="{id}">
+
+<div style="display: inline-block; width: 47%; ">
+{id} {line1}<br/>{line2}<br/>{line3}<br/>Author: {owner}<br/><audio controls><source src="{audio}" type="audio/mpeg">Your browser does not support this audio format.</audio>
+</div>
+<div style="display: inline-block; width: 47%; vertical-align: top; float: right;">
+<img src="images/form.png" style="max-height: 100px; max-width: 100px; float: right;"/>
+
+</div>
+
+</li>
+</script>
+
+
+<script>
+
+	YUI().use('node', function(Y) {
+	
+	var Dekaaz, 
+	query,
+	noTasksMessage = Y.one('#no-item-message'),
+	submitBtn = Y.one("#poem-submit"),
+	incompleteItemList = Y.one('#single-item'),
+	accountInfo = Y.one('body');
+
+	function populate(val, owner_param, owner_id_param) {
+		incompleteItemList.empty();
+		if(owner_id_param == null) {
+			
+			var content = Y.Lang.sub(Y.one('#todo-items-template-no-account').getHTML(), {
+				line1: val.get('line1'),
+				line2: val.get('line2'),
+				line3: val.get('line3'),
+				owner: owner_param,
+				createdAt: val.get('createdAt'),
+				audio: val.get('Sound'),
+				id: val.id,
+			});
+			incompleteItemList.prepend(content);
+		} else {
+			
+			var content = Y.Lang.sub(Y.one('#todo-items-template').getHTML(), {
+				line1: val.get('line1'),
+				line2: val.get('line2'),
+				line3: val.get('line3'),
+				owner: owner_param,
+				owner_id: owner_id_param,
+				createdAt: val.get('createdAt'),
+				audio: val.get('Sound'),
+				id: val.id,
+			});
+
+			
+			incompleteItemList.prepend(content);
+		}
+
+		// The following three variables are arrays of integers that hold the syllable
+		// counts for each word of each line of the Dekaaz
+		var first_line_syllables = val.get('line1syll');
+		var second_line_syllables = val.get('line2syll');
+		var third_line_syllables = val.get('line3syll');
+
+		// This line sets the image. I have set it to be Logo.png, but you can
+		// set the image with Processing, given the three arrays of int's directly above.
+		$('#single-item:first>li img').attr('src', 'images/Logo.png');
+	}
+
+<?php 
+	if (isset($_GET['dekaaz_id'])) {
+        echo 'var dekaaz_id="' . $_GET['dekaaz_id'] . '";';
+    }
+?>
+mDekaaz = Parse.Object.extend("Dekaaz");
+var mQuery = new Parse.Query(mDekaaz);
+mQuery.get(dekaaz_id, {
+  success: function(object) {
+    // console.log(object.get('line1'));
+
+	var author = object.get('parent');
+	// console.log(author.getUsername() + " ; " + author.id);
+	if(author != null) {
+		author.fetch({
+		  success: function(author) {
+		    var author_name = author.getUsername();
+		  	populate(object, author.getUsername(), author.id);
+		  	
+		  }
+		});
+	} else {
+		populate(object, "Unknown", null);
+	}
+
+  },
+
+  error: function(object, error) {
+    // error is an instance of Parse.Error.
+  }
+});
+
+});
+</script>
+<?php include_once("footer.php"); ?>
